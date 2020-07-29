866d769e2b7144d5138a393cc48014f993a81f71
Wellington Ribeiro <ligton.ribeiro@gmail.com>
2020-07-28 21:26:10 -0300

    ligtonfli Carousel com problemas

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import Button from '../Button';
 // import ButtonLink from './components/ButtonLink';
@@ -7,11 +8,11 @@ import './Menu.css'
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="LigtonFlix" />
-            </a>
+            </Link>
 
-            <Button as="a" href="/" className="ButtonLink">
+            <Button as={Link} to="/cadastro/video" className="ButtonLink">
                 Novo Vídeo
             </Button>
         </nav>
